9ab301a2b1ee0535436dd78ab3c286cb3604ab71
Agslz <agsp3rs3o@gmail.com>
2024-10-22 19:10:57 -0300

    Create StrConsumerListener.java

---
+++ str-consumer/src/main/java/com/ags/backend/listeners/StrConsumerListener.java
@@ -0,0 +1,28 @@
+package com.ags.backend.listeners;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+public class StrConsumerListener {
+
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = @TopicPartition(topic = "str-topic", partitions = {"0"}),
+            containerFactory = "strContainerFactory")
+    public void listener0(String message){
+        log.info("Recibiendo un mensaje {}", message);
+        log.info("Listener0 ::: Recibiendo un mensaje {}", message);
+    }
+
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = @TopicPartition(topic = "str-topic", partitions = {"1"}),
+            containerFactory = "strContainerFactory")
+    public void listener1(String message){
+        log.info("Recibiendo un mensaje {}", message);
+        log.info("Listener1 ::: Recibiendo un mensaje {}", message);
+    }
+
+}
